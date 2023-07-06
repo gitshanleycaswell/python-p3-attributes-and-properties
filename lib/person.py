97890c383f21e6e43d21e860798d4a92d861c468
Shanley Caswell <shanleycaswell@gmail.com>
2023-07-06 13:30:13 -0400

    tests passing

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Name", job="Sales"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, new_name):
+        if type(new_name) == str and 1 <= len(new_name) <= 25:
+            self._name = new_name.title()
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job  
+    
+    def set_job(self, new_job):
+        if new_job in APPROVED_JOBS:
+            self._job = new_job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
